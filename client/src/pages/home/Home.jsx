3b62b61b13b618d9bb6947f9750b36942d4f3d7d
josesales <salesbass@gmail.com>
2021-09-03 16:12:38 +0200

    Adding Loader component to Home Page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,28 +2,36 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router';
 import InputMessage from '../../components/input-message/InputMessage';
+import Loader from '../../components/loader/Loader';
 import Message from '../../components/message/Message';
 import Sidebar from '../../components/sidebar/Sidebar';
 import { HomeStyles, MainStyles } from './HomeStyles';
 const Home = () => {
 
     const user = useSelector(state => state.userReducer.user);
+    const room = useSelector(state => state.roomReducer.room);
 
     if (!user) {
         return <Redirect to="/login" />
     }
 
     return (
-        <HomeStyles>
+        <React.Fragment>
+            {
+                !room ? <Loader /> :
 
-            <Sidebar />
+                    <HomeStyles>
 
-            <MainStyles>
+                        <Sidebar />
 
-                <Message />
-                <InputMessage />
-            </MainStyles>
-        </HomeStyles>
+                        <MainStyles>
+
+                            <Message />
+                            <InputMessage />
+                        </MainStyles>
+                    </HomeStyles>
+            }
+        </React.Fragment>
     );
 
 }
