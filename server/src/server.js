0881283d1400d5a74259a33b21cdf7d778f02ba2
josesales <salesbass@gmail.com>
2021-09-04 14:46:15 +0200

    Build settings

--- server/src/server.js
+++ server/src/server.js
@@ -8,10 +8,10 @@ const { addUser, removeUserFromRoom, getUsersInRoom } = require('./services/user
 const app = express();
 
 if (process.env.NODE_ENV == 'production') {
-    app.use(express.static(path.join(__dirname, '../../client/build')))
-    app.get('/*', function (req, res) {
-        res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
-    });
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    // app.get('/*', function (req, res) {
+    //     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    //   });
 }
 
 const port = process.env.PORT || 5000;
